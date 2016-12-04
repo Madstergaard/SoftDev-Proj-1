0c63f6cac1a274fd6537e47c00c0c58e050af631
yawata159 <seijiyawata@gmail.com>
2016-12-04 16:40:28 -0500

    Added some comments

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, session, url_for, redirect
-import sqlite3, hashlib
 from utils import sift, sort, tix
 
 app = Flask(__name__)
@@ -11,8 +10,9 @@ def root():
 
 @app.route('/home/')
 def home():
+    # Check if logged in
     if 'access_token' in session:
-        profile = sift.get_profile_data(session['access_token']) #dict
+        profile = sift.get_profile_data(session['access_token'])
         saved_tracks = sift.get_saved_tracks(session['access_token'])
         return render_template(
                 'dashboard.html',
@@ -22,15 +22,20 @@ def home():
                 saved_tracks=saved_tracks
         )
     else:
+        # Authentication url is a Spotify page
         auth_url = sift.authentication_url()
         return render_template('dashboard.html', logged_in = False, auth_url=auth_url)
 
+# Used for the callback from Spotify to our website
 @app.route('/login/')
 def login():
+    # If it has been called back, then send a request to get an access token
+    # Might modify this (b.c. anyone could just put any code they wanted into the URL)
     if 'code' in request.args:
         auth_token = request.args['code']
         token_response = sift.token_request(auth_token)
         session['access_token'] = token_response['access_token']
+    # Always redirect to the homepage
     return redirect(url_for('home'))
 
 @app.route('/event/<eventID>')
--- utils/sift.py
+++ utils/sift.py
@@ -2,7 +2,9 @@
 import json # JSON (duh)
 import requests # GET and POST requests
 import base64 # Encoding client_id:client_secret
-import urllib # Encoding query parameters in url format (not provided in url2)
+import urllib # Encoding query parameters in url format (not provided in urllib2, sorry Mr. DW)
+
+# Authorization Code Flow implementation: https://developer.spotify.com/web-api/authorization-guide/#authorization-code-flow
 
 #  Client Keys
 CLIENT_ID = 'ff5c4964b91b47d0a40933042bbe51d4'
@@ -23,6 +25,7 @@ STATE = ''
 SHOW_DIALOG_bool = False
 SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
 
+# The parameter for the authentication url (the 'MOSSY wants to connect to your account' page)
 auth_query_params = {
     'response_type': 'code',
     'redirect_uri': REDIRECT_URI,
@@ -32,32 +35,51 @@ auth_query_params = {
     'client_id': CLIENT_ID
 }
 
+# Formats the params above into a nice url
 def authentication_url():
     url_args = urllib.urlencode(auth_query_params)
     auth_url = '{}/?{}'.format(SPOTIFY_AUTH_URL, url_args)
     return auth_url
 
+# After the user accepts the request from MOSSY, a POST request is made with the auth_token to obtain
+# an access_token and a refresh_token
 def token_request(auth_token):
+    # Request body params
     post_query_params = {
         'grant_type': 'authorization_code',
         'code': str(auth_token),
         'redirect_uri': REDIRECT_URI
     }
+    # Header Parameter (encodes the CLIENT_ID and CLIENT_SECRET)
     encoded_secret = base64.b64encode('{}:{}'.format(CLIENT_ID, CLIENT_SECRET))
     headers = {'Authorization': 'Basic {}'.format(encoded_secret)}
+    # POST request
     post_request = requests.post(SPOTIFY_TOKEN_URL, data=post_query_params, headers=headers)
-
+    # Response JSON data with 'access_token', 'token_type', 'scope', 'expires_in' and 'refresh_token'
+    # **Right now the app's only using the access_token, but once it expires, there's no fallback
     response_data = json.loads(post_request.text)
     return response_data
 
+# Reuqests to the Spotify API (not the authorization one) that access a user's info (which we are)
+# require the access_token in their request headers
+# Possible endpoints listed at https://developer.spotify.com/web-api/endpoint-reference/
+# The two below are just examples (they can be extended to any other kind of data we might need)
+
 def get_profile_data(access_token):
+    # Include access_token in header
     authorization_header = {'authorization': 'Bearer {}'.format(access_token)}
+    # The URL to get JSON data from
     user_profile_api_endpoint = '{}/me'.format(SPOTIFY_API_URL)
+    # GET request to the url (incl the header)
     profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    # Raw JSON string
     profile_json_text = profile_response.text
+    # Dictionary with JSON data
     profile_dict = json.loads(profile_json_text)
     return profile_dict
 
+# Same thing but with a different URL endpoint (these could probably be incorporated into a
+# single function that just takes the URL endpoint)
 def get_saved_tracks(access_token):
     authorization_header = {'authorization': 'Bearer {}'.format(access_token)}
     tracks_api_endpoint = '{}/me/tracks'.format(SPOTIFY_API_URL)
