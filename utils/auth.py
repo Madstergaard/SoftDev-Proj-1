d3d0ceafb6ba3fdc4cab9bb01c1b93bfca241d56
yawata159 <seijiyawata@gmail.com>
2016-12-05 19:56:31 -0500

    Separated sift.py into two files (auth and sift) - Implement state param to the authentication process - Display all saved tracks (no need for upper bound limit) - Still need to figure out refresh token (refresh after every request?) - Since all the functions to sift through the tracks (in app.py) keep referring to previous functions, we should save all the info we need from the user after we log in, and then get our info from then. Right now, what we have is very unwieldy - sift.py has new function data, which just takes an API endpoint and returns its json data. - top_artists now returns an ordered dict instead of a list of tuples - Dashboard now displays all saved tracks and the top 10 artists (by number of tracks saved)

---
+++ utils/auth.py
@@ -0,0 +1,60 @@
+import json # JSON (duh)
+import requests # GET and POST requests
+import base64 # Encoding client_id:client_secret
+import urllib # Encoding query parameters in url format (not provided in urllib2, sorry Mr. DW)
+import uuid # For generating unique identifier (state param)
+
+# Authorization Code Flow implementation: https://developer.spotify.com/web-api/authorization-guide/#authorization-code-flow
+
+# Client Keys
+CLIENT_ID = 'ff5c4964b91b47d0a40933042bbe51d4'
+CLIENT_SECRET = '034ac7a503834cd9a02f25183c3397d6'
+
+# Spotify URLS
+SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
+SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+
+# Server-side Parameters
+REDIRECT_URI = 'http://127.0.0.1:5000/login/'
+SCOPE = 'playlist-read-private user-follow-read user-library-read'
+STATE = str(uuid.uuid4())
+SHOW_DIALOG_bool = False
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+# The parameter for the authentication url (the 'MOSSY wants to connect to your account' page)
+auth_query_params = {
+    'response_type': 'code',
+    'redirect_uri': REDIRECT_URI,
+    'scope': SCOPE,
+    'state': STATE,
+    'show_dialog': SHOW_DIALOG_str,
+    'client_id': CLIENT_ID
+}
+
+# Formats the params above into a nice url
+def authentication_url():
+    url_args = urllib.urlencode(auth_query_params)
+    auth_url = '{}/?{}'.format(SPOTIFY_AUTH_URL, url_args)
+    return auth_url
+
+def check_state(state):
+    return state == STATE
+
+# After the user accepts the request from MOSSY, a POST request is made with the auth_token to obtain
+# an access_token and a refresh_token
+def token_request(auth_token):
+    # Request body params
+    post_query_params = {
+        'grant_type': 'authorization_code',
+        'code': str(auth_token),
+        'redirect_uri': REDIRECT_URI
+    }
+    # Header Parameter (encodes the CLIENT_ID and CLIENT_SECRET)
+    encoded_secret = base64.b64encode('{}:{}'.format(CLIENT_ID, CLIENT_SECRET))
+    headers = {'Authorization': 'Basic {}'.format(encoded_secret)}
+    # POST request
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=post_query_params, headers=headers)
+    # Response JSON data with 'access_token', 'token_type', 'scope', 'expires_in' and 'refresh_token'
+    # **Right now the app's only using the access_token, but once it expires, there's no fallback
+    response_data = json.loads(post_request.text)
+    return response_data
