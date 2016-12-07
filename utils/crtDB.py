8e2e4677b8b4a788909c933958cfd8cc7fd99c3a
Maddie Ostergaard <Mostergaard@stuy.edu>
2016-12-06 23:37:33 -0500

    Maddie out

--- utils/crtDB.py
+++ utils/crtDB.py
@@ -10,10 +10,10 @@ c = db.cursor()
 userTable = "CREATE TABLE Users(username TEXT, location TEXT, numArtists INTEGER);"
 c.execute(userTable)
 
-goingTable = "CREATE TABLE EventsAttending(username TEXT, eventName TEXT, location TEXT);"
+goingTable = "CREATE TABLE EventsAttending(username TEXT, eventName TEXT, eventURL TEXT, location TEXT, dateOfEvent TEXT);"
 c.execute(goingTable)
 
-interestedTable = "CREATE TABLE EventsInterestedIn(username TEXT, eventName TEXT, location TEXT);"
+interestedTable = "CREATE TABLE EventsInterestedIn(username TEXT, eventName TEXT, eventURL TEXT, location TEXT, dateOfEvent TEXT);"
 c.execute(interestedTable)
 
 db.commit()
