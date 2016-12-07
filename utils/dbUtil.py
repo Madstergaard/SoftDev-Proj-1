dd11258dee3e751425e374318e89829022731346
Maddie Ostergaard <Mostergaard@stuy.edu>
2016-12-06 23:19:52 -0500

    log out and username fixes

--- utils/dbUtil.py
+++ utils/dbUtil.py
@@ -1,11 +1,12 @@
 import sqlite3, sift
+from flask import session
 
 def isUserInDB():
 	db = sqlite3.connect("data/DB.db")
 	c = db.cursor()
 
 	# use oAuth to retrieve username
-	user = sift.profile_data("access_token").get('id')
+	user = sift.profile_data(session["access_token"]).get('id')
 
     # Check if username in DB
 	cmd = "SELECT * FROM Users;"
@@ -23,17 +24,18 @@ def addUserToDB():
 	c = db.cursor()
 
     # use Spotify API to retrieve username
-	#user = sift.profile_data("access_token").get('id')
-	user = "Test"
+	user = sift.profile_data(session["access_token"]).get('id')
+	# user = "Test1"
 
     # use Ipinfo to get location
-	#location = "this hasn't been implemented yet"
-	location = "Test"
+	location = "this hasn't been implemented yet"
+	# location = "Test1"
 
     # default preferences
 	numArtists = 10
 
-    # Check if username in DB
-	cmd = "INSERT INTO Users VALUES('%s', '%s', %d);" %(user, location, numArtists)
+	cmd = 'INSERT INTO Users VALUES("%s", "%s", %d);' %(user, location, numArtists)
+	c.execute(cmd)
+	db.commit()
 	db.close()
 	return 
