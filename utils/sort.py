76011dab268b4a3963ee2f26cf6e285f90cd3d26
Sarah Yoon <syoon2@stuy.edu>
2016-12-08 02:29:36 -0500

    wrote sort_by_status, haven't tested

--- utils/sort.py
+++ utils/sort.py
@@ -6,3 +6,18 @@ def sort_by_date(event_list):
     ret = sorted(event_list, key=lambda k: k['date'])
     return ret
 
+# Takes event list, groups by status
+# Returns dict of three separate event lists (going/interested/unmarked)
+# Still hasn't been tested because we haven't implemented statuses yet
+def sort_by_status(event_list):
+    inorder = sorted(event_list, key=lambda k: k['status'])
+    ret = {}
+    for eventdict in inorder:
+        status = eventdict['status']
+        if status in inorder:
+            ret['status'].append(eventdict)
+        else:
+            ret['status'] = [eventdict]
+    for group in ret:
+        group = sort_by_date(group)
+    return ret
